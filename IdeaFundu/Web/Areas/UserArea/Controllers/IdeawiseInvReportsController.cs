585fa04830bf1c52d49d01d62bc4a978d91a6ad2
Shivam Ingle <ingleshivam@gmail.com>
2024-05-19 10:16:12 +0530

    19-05-2024

---
+++ IdeaFundu/Web/Areas/UserArea/Controllers/IdeawiseInvReportsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Repository;
+using Web.CustFilter;
+
+namespace Web.Areas.UserArea.Controllers
+{
+    [Area("UserArea")]
+    [UserAuthroization]
+    public class IdeawiseInvReportsController : Controller
+    {
+        IIdea IdeaRepo;
+        public IdeawiseInvReportsController(IIdea _IdeaRepo)
+        {
+            IdeaRepo = _IdeaRepo;
+        }
+        public IActionResult Index()
+        {
+            Int64 UserID = Convert.ToInt64(HttpContext.Session.GetString("UserID"));
+            ViewBag.IdeaID = new SelectList(this.IdeaRepo.GetAllByUserID(UserID),"IdeaID","IdeaName");
+            return View();
+        }
+
+        public IActionResult IdeaWiseInvReportsAction(Int64 IdeaID)
+        {
+            return View();
+        }
+    }
+}
